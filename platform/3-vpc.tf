c33390ef3fa7db0af9535db6f559995e12531972
EimusAnan <eimus.anan@gmail.com>
2026-05-01 14:17:40 -0400

    Reestablishing default route.

--- platform/3-vpc.tf
+++ platform/3-vpc.tf
@@ -15,18 +15,19 @@ resource "google_compute_network" "vpc" {
 #   }
 # }
 
-# # Remove this route to make the VPC fully private.check
-# # You need this route for the NAT gateway.
-# resource "google_compute_route" "default_route" {
-#   name             = "default-route"
-#   dest_range       = "0.0.0.0/0"
-#   network          = google_compute_network.vpc.name
-#   next_hop_gateway = "default-internet-gateway"
-# }
-removed {
-  from = google_compute_route.default_route
-
-  lifecycle {
-    destroy = true
-  }
+# Remove this route to make the VPC fully private.check
+# You need this route for the NAT gateway.
+resource "google_compute_route" "default_route" {
+  name             = "default-route"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc.name
+  next_hop_gateway = "default-internet-gateway"
 }
+
+# removed {
+#   from = google_compute_route.default_route
+
+#   lifecycle {
+#     destroy = true
+#   }
+# }
