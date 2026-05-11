b1fe75a377f519171a502110d1c9c5baf2b2e9f7
EimusAnan <eimus.anan@gmail.com>
2026-05-11 07:13:56 -0400

    Set deletion protection to false for the assured folder.

--- platform/2-folder.tf
+++ platform/2-folder.tf
@@ -1,4 +1,5 @@
 resource "google_folder" "assured" {
-  display_name = "Assured Folder"
-  parent       = var.org_id
+  display_name        = "Assured Folder"
+  parent              = var.org_id
+  deletion_protection = false
 }
