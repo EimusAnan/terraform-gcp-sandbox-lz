c98e26220ee823c81138ef0df35691973735ee05
EimusAnan <eimus.anan@gmail.com>
2026-05-11 07:07:18 -0400

    Additional variable updates locals > input variables.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 module "platform" {
   source         = "./platform"
   org_id         = "940510183197"
-  project_id     = "cs-poc-wimfmwwzffpbmpliqqio4g4"
-  region         = "us-central1"
+  project_id     = locals.project_id
+  region         = locals.region
   api_dependency = google_project_service.api
 }
--- variables.tf
+++ variables.tf
@@ -1,4 +1,6 @@
 locals {
+  project_id = "cs-poc-wimfmwwzffpbmpliqqio4g4"
+  region     = "us-central1"
   apis = [
     "compute.googleapis.com",
     "logging.googleapis.com",
